b5de1277f93dfbd3a45630df8c45561bf0f82eb8
Zakaria Santoso <santosozakaria29@gmail.com>
2022-02-18 15:40:11 +0700

    added post employee data

--- server/index.js
+++ server/index.js
@@ -4,6 +4,19 @@ const PORT = 3000;
 const createEmployeeTable = require("./db/EmployeeStorage");
 const db = require("./db/index");
 
+app.use(express.json());
+
+app.post("/api/employees", async (req, res, next) => {
+  try {
+    const { name, email, phone, position, salary } = req.body;
+    const result = await db.query("INSERT INTO employees (name, email, phone, position, salary) VALUES ($1, $2, $3, $4, $5) RETURNING *", [name, email, phone, position, salary]);
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
 app.get("/", async (req, res) => {
   res.send("Hello World! V2");
 });
@@ -12,6 +25,17 @@ app.get("/api/employees", async (req, res, next) => {
   try {
     let sql = "SELECT * FROM employees";
     let results = await (await db.query(sql)).rows;
+    res.send(results);
+  } catch (error) {
+    console.log(error);
+    next(error); // pass error to express error handler
+  }
+});
+
+app.get("/api/employees/:id", async (req, res, next) => {
+  try {
+    let sql = "SELECT * FROM employees WHERE id = $1";
+    let results = await (await db.query(sql, [req.params.id])).rows;
     res.send(results);
   } catch (error) {
     console.log(error);
