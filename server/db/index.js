a3be0e8033f86136da3eca45b98407bc877a8a8d
Zakaria Santoso <santosozakaria29@gmail.com>
2022-02-18 11:20:13 +0700

    added postgrees database connection pool

---
+++ server/db/index.js
@@ -0,0 +1,19 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+module.exports = {
+  async query(text, params) {
+    const start = Date.now();
+    const res = await pool.query(text, params);
+    const duration = Date.now() - start;
+    console.log(`Query execution time: ${duration}ms`, { text, params, rows: res.rowCount });
+  },
+};
